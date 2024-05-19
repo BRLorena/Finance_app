c8bdce86db80fe82e0ddfaced74f4b7b61f71500
Bruno Lorena <brunomlorena95@gmail.com>
2024-05-19 20:28:52 +0100

    Added flasgger to document the API

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flasgger import Swagger, swag_from
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 # Sample data structure to store data
 invoices = {}
@@ -9,12 +11,46 @@ balance = 0
 
 
 @app.route("/balance", methods=["GET"])
+@swag_from(
+    {
+        "responses": {
+            200: {
+                "description": "Get current balance",
+                "schema": {
+                    "type": "object",
+                    "properties": {"balance": {"type": "number"}},
+                },
+            }
+        }
+    }
+)
 def get_balance():
     global balance
     return jsonify({"balance": balance})
 
 
 @app.route("/invoice", methods=["POST"])
+@swag_from(
+    {
+        "parameters": [
+            {
+                "name": "body",
+                "in": "body",
+                "required": True,
+                "schema": {
+                    "type": "object",
+                    "properties": {"amount": {"type": "number"}},
+                },
+            }
+        ],
+        "responses": {
+            201: {
+                "description": "Invoice created",
+                "schema": {"type": "object", "properties": {"id": {"type": "integer"}}},
+            }
+        },
+    }
+)
 def create_invoice():
     global balance
     invoice = request.get_json()
@@ -25,6 +61,38 @@ def create_invoice():
 
 
 @app.route("/invoice/<int:id>", methods=["PUT"])
+@swag_from(
+    {
+        "parameters": [
+            {
+                "name": "id",
+                "in": "path",
+                "type": "integer",
+                "required": True,
+                "description": "ID of the invoice to update",
+            },
+            {
+                "name": "body",
+                "in": "body",
+                "required": True,
+                "schema": {
+                    "type": "object",
+                    "properties": {"amount": {"type": "number"}},
+                },
+            },
+        ],
+        "responses": {
+            200: {
+                "description": "Invoice updated",
+                "schema": {
+                    "type": "object",
+                    "properties": {"amount": {"type": "number"}},
+                },
+            },
+            404: {"description": "Invoice not found"},
+        },
+    }
+)
 def update_invoice(id):
     global balance
     if id in invoices:
@@ -37,6 +105,23 @@ def update_invoice(id):
 
 
 @app.route("/invoice/<int:id>", methods=["DELETE"])
+@swag_from(
+    {
+        "parameters": [
+            {
+                "name": "id",
+                "in": "path",
+                "type": "integer",
+                "required": True,
+                "description": "ID of the invoice to delete",
+            }
+        ],
+        "responses": {
+            204: {"description": "Invoice deleted"},
+            404: {"description": "Invoice not found"},
+        },
+    }
+)
 def delete_invoice(id):
     global balance
     if id in invoices:
@@ -47,6 +132,27 @@ def delete_invoice(id):
 
 
 @app.route("/income", methods=["POST"])
+@swag_from(
+    {
+        "parameters": [
+            {
+                "name": "body",
+                "in": "body",
+                "required": True,
+                "schema": {
+                    "type": "object",
+                    "properties": {"amount": {"type": "number"}},
+                },
+            }
+        ],
+        "responses": {
+            201: {
+                "description": "Income created",
+                "schema": {"type": "object", "properties": {"id": {"type": "integer"}}},
+            }
+        },
+    }
+)
 def create_income():
     global balance
     income = request.get_json()
@@ -56,6 +162,19 @@ def create_income():
 
 
 @app.route("/total-invoice", methods=["GET"])
+@swag_from(
+    {
+        "responses": {
+            200: {
+                "description": "Get total amount of all invoices",
+                "schema": {
+                    "type": "object",
+                    "properties": {"total_invoice": {"type": "number"}},
+                },
+            }
+        }
+    }
+)
 def get_total_invoice():
     total = sum(invoice["amount"] for invoice in invoices.values())
     return jsonify({"total_invoice": total})
